64efb4a04169ebfc1c41be5cc1260aaecc613457
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-03-06 21:34:34 +0200

    Fixed braunches

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,60 @@
 /**
  * Created by tomer on 1/29/16.
  */
-var app = angular.module('GuyKoren', ['ngMaterial']).config(function($mdThemingProvider) {
+var app = angular.module('GuyKoren', ['ngMaterial']).config(function ($mdThemingProvider) {
     // Extend the red theme with a few different colors
     //create yr own palette
 
 });
 
-app.controller('AppController', function ($scope, $mdDialog, $mdBottomSheet,$mdMedia) {
-    $scope.$watch(function() { return $mdMedia('gt-sm'); }, function(big) {
+app.controller('AppController', function ($scope, $mdDialog, $mdBottomSheet, $mdMedia) {
+    $scope.$watch(function () {
+        return $mdMedia('gt-sm');
+    }, function (big) {
         $scope.bigScreen = big;
     });
     $scope.extra_about = false;
-    $scope.toggleAboutExtra = function(){
+    $scope.toggleAboutExtra = function () {
         console.log("click")
         $scope.extra_about = !$scope.extra_about;
-    }
-    $scope.braunches = ["מרכזר כרמים: היקב 13","מרכז הנחלה: העצמאות 45","מרכז התדר: הרצל 136","סניף המשתלה אלי תבין 8","בית ספר הישגים: חזית חמש 2","בית ספר הישגים: מלחמת ששת הימים 23","עירוני יד - בקרוב"]
-    $scope.books = [1,2,3,4,5,8,9,10]
+    };
+    $scope.braunches = [
+        {
+            text: "ראשון לציון",
+            title: true
+
+        },
+        {
+            text: "מרכז כרמים: היקב 13"
+        },
+        {
+            text: "מרכז הנחלה: העצמאות 45"
+        },
+        {
+            text: "מרכז התדר: הרצל 136"
+        },
+        {
+            text: "תל אביב",
+            title: true
+        },
+        {
+            text: "סניף המשתלה אלי תבין 8"
+        },
+        {
+            text: "עירוני יד - בקרוב"
+        },
+        {
+            text: "חולון",
+            title: true
+        },
+        {
+            text: "בית ספר הישגים: חזית חמש 2"
+        },
+        {
+            text: "בית ספר הישגים: מלחמת ששת הימים 23"
+        }
+    ];
+    $scope.books = [1, 2, 3, 4, 5, 8, 9, 10];
 
     $scope.plans = [
         {
