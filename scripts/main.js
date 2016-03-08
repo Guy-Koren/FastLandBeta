846ffd9646091bee60349529fe94c2d037586512
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-03-08 20:18:32 +0200

    Added materials

--- scripts/main.js
+++ scripts/main.js
@@ -95,7 +95,166 @@ app.controller('AppController', function ($scope, $mdDialog, $mdBottomSheet, $md
             text: "הכנה לבגרויות/מרתונים",
             icon: "document"
         }
-    ]
+    ];
+
+    $scope.stuff = [
+        /*  {
+         grade: "ז",
+         material: [
+         {
+         title: "משוואות – כיתה ז' מבחן מפמר לכיתה ז' תשעב – מופת – טור א",
+         link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%A9%D7%95%D7%95%D7%90%D7%95%D7%AA-%D7%9B%D7%99%D7%AA%D7%94-%D7%96.pdf"
+         }
+         ]
+         },*/
+        {
+            grade: "כיתה ח",
+            material: [
+                {
+                    title: "מבחן כיתה ח' נחלה 4.12.14",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%91%D7%97%D7%9F-%D7%9B%D7%99%D7%AA%D7%94-%D7%97-%D7%A0%D7%97%D7%9C%D7%94-4.12.14.pdf"
+                },
+                {
+                    title: "פונקציה קווית – תרגילים",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A4%D7%95%D7%A0%D7%A7%D7%A6%D7%99%D7%94-%D7%A7%D7%95%D7%95%D7%99%D7%AA-%D7%AA%D7%A8%D7%92%D7%99%D7%9C%D7%99%D7%9D2.pdf"
+                },
+                {
+                    title: "פונקציה קווית – מתקדם",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A4%D7%95%D7%A0%D7%A7%D7%A6%D7%99%D7%94-%D7%A7%D7%95%D7%95%D7%99%D7%AA-%D7%9E%D7%AA%D7%A7%D7%93%D7%9D1.pdf"
+                },
+                {
+                    title: "חפיפת משולשים + קווים מקבילים – כיתה ח'",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97%D7%A4%D7%99%D7%A4%D7%AA-%D7%9E%D7%A9%D7%95%D7%9C%D7%A9%D7%99%D7%9D-+-%D7%A7%D7%95%D7%95%D7%99%D7%9D-%D7%9E%D7%A7%D7%91%D7%99%D7%9C%D7%99%D7%9D-%D7%9B%D7%99%D7%AA%D7%94-%D7%97.pdf"
+                },
+                {
+                    title: "חפיפת משולשים וקטעים מיוחדים במשולש – כיתה ח",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97%D7%A4%D7%99%D7%A4%D7%AA-%D7%9E%D7%A9%D7%95%D7%9C%D7%A9%D7%99%D7%A4-%D7%95%D7%A7%D7%98%D7%A2%D7%99%D7%9D-%D7%9E%D7%99%D7%95%D7%97%D7%93%D7%99%D7%9D-%D7%91%D7%9E%D7%A9%D7%95%D7%9C%D7%A9-%D7%9B%D7%99%D7%AA%D7%94-%D7%97.pdf"
+                },
+                {
+                    title: "משולש שווה שוקים – כיתה ח'",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%A9%D7%95%D7%9C%D7%A9-%D7%A9%D7%95%D7%95%D7%94-%D7%A9%D7%95%D7%A7%D7%99%D7%9D-%D7%9B%D7%99%D7%AA%D7%94-%D7%97.pdf"
+                }
+            ]
+        },
+        {
+            grade: "כיתה ט",
+            material: [
+                {
+                    title: "חזקות – צימצום שברים",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97%D7%96%D7%A7%D7%95%D7%AA-%D7%A6%D7%99%D7%9E%D7%A6%D7%95%D7%9D-%D7%A9%D7%91%D7%A8%D7%99%D7%9D.pdf"
+                },
+                {
+                    title: "פירוק לגורמים",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97%D7%96%D7%A7%D7%95%D7%AA-%D7%A6%D7%99%D7%9E%D7%A6%D7%95%D7%9D-%D7%A9%D7%91%D7%A8%D7%99%D7%9D.pdf"
+                },
+                {
+                    title: "צימצום שברים אלגברים",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A6%D7%99%D7%9E%D7%A6%D7%95%D7%9D-%D7%A9%D7%91%D7%A8%D7%99%D7%9D-%D7%90%D7%9C%D7%92%D7%91%D7%A8%D7%99%D7%9D3.pdf"
+                },
+                {
+                    title: "פרבולה – תרגילים",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A4%D7%A8%D7%91%D7%95%D7%9C%D7%94-%D7%AA%D7%A8%D7%92%D7%99%D7%9C%D7%99%D7%9D.pdf"
+                },
+                {
+                    title: "תרגילי פרבולה – כיתה ט' – רמה קושי גבוה",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%AA%D7%A8%D7%92%D7%99%D7%9C%D7%99-%D7%A4%D7%A8%D7%91%D7%95%D7%9C%D7%94-%D7%9B%D7%99%D7%AA%D7%94-%D7%98-%D7%A8%D7%9E%D7%94-%D7%A7%D7%95%D7%A9%D7%99-%D7%92%D7%91%D7%95%D7%94-%D7%92%D7%99%D7%90-%D7%A7%D7%95%D7%A8%D7%9F1.pdf"
+                },
+                {
+                    title: "פרבולה – מעבר הצגות",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A4%D7%A8%D7%91%D7%95%D7%9C%D7%94-%D7%9E%D7%A2%D7%91%D7%A8-%D7%94%D7%A6%D7%92%D7%95%D7%AA1.pdf"
+                }
+            ]
+        },
+        {
+            grade: "כיתה י",
+            material: [
+                {
+                    title: "טריגו וגיאומטריה",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%98%D7%A8%D7%99%D7%92%D7%95-%D7%95%D7%92%D7%99%D7%90%D7%95%D7%9E%D7%98%D7%A8%D7%99%D7%94.pdf"
+                },
+                {
+                    title: "חשבון דיפרנציאלי – הנגזרת של הפונקציה – גיא קורן",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97%D7%A9%D7%91%D7%95%D7%9F-%D7%93%D7%99%D7%A4%D7%A8%D7%A0%D7%A6%D7%99%D7%90%D7%9C%D7%99-%D7%94%D7%A0%D7%92%D7%96%D7%A8%D7%AA-%D7%A9%D7%9C-%D7%94%D7%A4%D7%95%D7%A0%D7%A7%D7%A6%D7%99%D7%94-%D7%92%D7%99%D7%90-%D7%A7%D7%95%D7%A8%D7%9F.pdf"
+                },
+                {
+                    title: "סיכום שיעור גיאומטריה – מתמטיקה מוגבר",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A1%D7%99%D7%9B%D7%95%D7%9D-%D7%A9%D7%99%D7%A2%D7%95%D7%A8-%D7%9E%D7%AA%D7%9E%D7%98%D7%99%D7%A7%D7%94-%D7%9E%D7%95%D7%92%D7%91%D7%A8.pdf"
+                }
+            ]
+        },
+        {
+            grade: "3 יחידות",
+            material: [
+                {
+                    title: "טריגונומטריה – תרגילי בסיס",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%98%D7%A8%D7%99%D7%92%D7%95%D7%A0%D7%95%D7%9E%D7%98%D7%A8%D7%99%D7%94-%D7%AA%D7%A8%D7%92%D7%99%D7%9C%D7%99-%D7%91%D7%A1%D7%99%D7%A1.pdf"
+                },
+                {
+                    title: "מבחן מס 1 801",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%91%D7%97%D7%9F-%D7%9E%D7%A1-1-801.pdf"
+                },
+                {
+                    title: "מבחן לדוגמא 1 802",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%91%D7%97%D7%9F-%D7%9C%D7%93%D7%95%D7%92%D7%9E%D7%90-1-8021.pdf"
+                },
+                {
+                    title: "ח' דיפרנצאלי – פונקציית שורש 803",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%97-%D7%93%D7%99%D7%A4%D7%A8%D7%A0%D7%A6%D7%90%D7%9C%D7%99-%D7%A4%D7%95%D7%A0%D7%A7%D7%A6%D7%99%D7%99%D7%AA-%D7%A9%D7%95%D7%A8%D7%A9-803.pdf"
+                },
+                {
+                    title: " תרגילי בגרות 803",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%AA%D7%A8%D7%92%D7%99%D7%9C%D7%99-%D7%91%D7%92%D7%A8%D7%95%D7%AA-803.pdf"
+                },
+                {
+                    title: "דף נוסחאות במתמטיקה 3 יח'",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%93%D7%A3-%D7%A0%D7%95%D7%A1%D7%97%D7%90%D7%95%D7%AA-%D7%91%D7%9E%D7%AA%D7%9E%D7%98%D7%99%D7%A7%D7%94-3-%D7%99%D7%971.pdf"
+                }
+
+            ]
+        },
+        {
+            grade: "4 יחידות",
+            material: [
+                {
+                    title: "משפטי מעגל",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%A9%D7%A4%D7%98%D7%99-%D7%9E%D7%A2%D7%92%D7%9C.pdf"
+                },
+                {
+                    title: "מבחן 804",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%91%D7%97%D7%9F-%D7%9E%D7%A1-1-26.11.2014.docx"
+                },
+                {
+                    title: "מבחן 805",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%91%D7%97%D7%9F-%D7%9E%D7%A1-2-805-%D7%A0%D7%97%D7%9C%D7%94-21.10.20141.pdf"
+                }
+            ]
+        },
+        {
+            grade: "5 יחידות",
+            material: [
+                {
+                    title: "דף חזרה למבחן 806 נחלה 13קיץ 2012 ב 35806",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%93%D7%A3-%D7%97%D7%96%D7%A8%D7%94-%D7%9C%D7%9E%D7%91%D7%97%D7%9F-806-%D7%A0%D7%97%D7%9C%D7%94-13.pdf"
+                },
+                {
+                    title: "מבחן 806 דוגמא",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%A7%D7%99%D7%A5-2012-35806.pdf"
+                },
+                {
+                    title: "משפטים בגאומטריה",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%9E%D7%A9%D7%A4%D7%98%D7%99%D7%9D-%D7%91%D7%92%D7%90%D7%95%D7%9E%D7%98%D7%A8%D7%99%D7%94.pdf"
+                },
+                {
+                    title: "Trigonometry formulas",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/Trigonometry-formulas.pdf"
+                },
+                {
+                    title: "דף נוסחאות 5 יחידות",
+                    link: "http://korenguy.com/wp-content/uploads/2013/03/%D7%93%D7%A3-%D7%A0%D7%95%D7%A1%D7%97%D7%90%D7%95%D7%AA-5-%D7%99%D7%97%D7%99%D7%93%D7%95%D7%AA.pdf"
+                }
+            ]
+        }
+    ];
 
 
     $scope.showAdvanced = function (ev) {
@@ -123,7 +282,7 @@ app.controller('AppController', function ($scope, $mdDialog, $mdBottomSheet, $md
 
     };
     $scope.showAdvanced(null);
-    setTimeout(function(){
+    setTimeout(function () {
         $mdDialog.cancel();
-    },4000)
+    }, 4000)
 });
